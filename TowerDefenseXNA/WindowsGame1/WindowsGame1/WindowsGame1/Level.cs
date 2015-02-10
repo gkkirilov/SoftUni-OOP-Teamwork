93196d311f763a8acfc733c21598f48b694e9593
Daniel-Georgiev <daniko91@mail.bg>
2015-02-10 16:49:24 +0200

    TowerDefenseXNA

---
+++ TowerDefenseXNA/WindowsGame1/WindowsGame1/WindowsGame1/Level.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WindowsGame1
+{
+    public class Level
+    {
+        int[,] map = new int[,] 
+        {
+            {0,0,0,0,0,0,0,1,0,0,0,0,0,0,},
+            {0,1,1,1,1,1,1,1,0,0,0,0,0,0,},
+            {0,1,0,0,0,0,0,0,0,0,0,0,0,0,},
+            {0,1,0,1,1,1,1,1,1,1,1,1,1,0,},
+            {0,1,0,1,0,0,0,0,0,0,0,0,1,0,},
+            {0,1,0,1,0,1,1,1,1,1,0,0,1,0,},
+            {0,1,0,1,0,1,0,0,0,1,0,0,1,0,},
+            {0,1,0,1,0,1,0,0,0,1,0,0,1,0,},
+            {0,1,0,1,0,1,0,0,0,1,0,0,1,0,},
+            {0,1,0,1,0,1,0,0,0,1,0,0,1,0,},
+            {0,1,0,1,1,1,0,0,0,1,0,0,1,0,},
+            {0,1,0,0,0,0,0,0,0,1,0,0,1,0,},
+            {0,1,1,1,1,1,1,1,1,1,0,0,1,0,},
+            {0,0,0,0,0,0,0,0,0,0,0,0,1,0,}
+
+        };
+
+        public int Width
+        {
+            get { return map.GetLength(1); }
+        }
+        public int Height
+        {
+            get { return map.GetLength(0); }
+        }
+
+        private List<Texture2D> tileTextures = new List<Texture2D>();
+        private Queue<Vector2> waypoints = new Queue<Vector2>();
+        public Level()
+        {
+            waypoints.Enqueue(new Vector2(7, 0) * 32);
+            waypoints.Enqueue(new Vector2(7, 1) * 32);
+            waypoints.Enqueue(new Vector2(1, 1) * 32);
+            waypoints.Enqueue(new Vector2(1, 2) * 32);
+            waypoints.Enqueue(new Vector2(1, 2) * 32);
+            waypoints.Enqueue(new Vector2(1, 4) * 32);
+            waypoints.Enqueue(new Vector2(1, 4) * 32);
+            waypoints.Enqueue(new Vector2(1, 5) * 32);
+            waypoints.Enqueue(new Vector2(1, 5) * 32);
+            waypoints.Enqueue(new Vector2(1, 7) * 32);
+            waypoints.Enqueue(new Vector2(1, 8) * 32);
+            waypoints.Enqueue(new Vector2(1, 9) * 32);
+            waypoints.Enqueue(new Vector2(1, 10) * 32);
+            waypoints.Enqueue(new Vector2(1, 11) * 32);
+            waypoints.Enqueue(new Vector2(1, 12) * 32);
+            waypoints.Enqueue(new Vector2(2, 12) * 32);
+            waypoints.Enqueue(new Vector2(3, 12) * 32);
+            waypoints.Enqueue(new Vector2(4, 12) * 32);
+            waypoints.Enqueue(new Vector2(5, 12) * 32);
+            waypoints.Enqueue(new Vector2(6, 12) * 32);
+            waypoints.Enqueue(new Vector2(7, 12) * 32);
+            waypoints.Enqueue(new Vector2(8, 12) * 32);
+            waypoints.Enqueue(new Vector2(9, 12) * 32);
+            waypoints.Enqueue(new Vector2(9, 11) * 32);
+            waypoints.Enqueue(new Vector2(9, 10) * 32);
+            waypoints.Enqueue(new Vector2(9, 9) * 32);
+            waypoints.Enqueue(new Vector2(9, 8) * 32);
+            waypoints.Enqueue(new Vector2(9, 7) * 32);
+            waypoints.Enqueue(new Vector2(9, 6) * 32);
+            waypoints.Enqueue(new Vector2(9, 5) * 32);
+            waypoints.Enqueue(new Vector2(8, 5) * 32);
+            waypoints.Enqueue(new Vector2(7, 5) * 32);
+            waypoints.Enqueue(new Vector2(6, 5) * 32);
+            waypoints.Enqueue(new Vector2(5, 5) * 32);
+            waypoints.Enqueue(new Vector2(5, 6) * 32);
+            waypoints.Enqueue(new Vector2(5, 7) * 32);
+            waypoints.Enqueue(new Vector2(5, 8) * 32);
+            waypoints.Enqueue(new Vector2(5, 9) * 32);
+            waypoints.Enqueue(new Vector2(5, 10) * 32);
+            waypoints.Enqueue(new Vector2(4, 10) * 32);
+            waypoints.Enqueue(new Vector2(3, 10) * 32);
+            waypoints.Enqueue(new Vector2(3, 9) * 32);
+            waypoints.Enqueue(new Vector2(3, 8) * 32);
+            waypoints.Enqueue(new Vector2(3, 7) * 32);
+            waypoints.Enqueue(new Vector2(3, 6) * 32);
+            waypoints.Enqueue(new Vector2(3, 5) * 32);
+            waypoints.Enqueue(new Vector2(3, 4) * 32);
+            waypoints.Enqueue(new Vector2(3, 3) * 32);
+            waypoints.Enqueue(new Vector2(4, 3) * 32);
+            waypoints.Enqueue(new Vector2(5, 3) * 32);
+            waypoints.Enqueue(new Vector2(6, 3) * 32);
+            waypoints.Enqueue(new Vector2(7, 3) * 32);
+            waypoints.Enqueue(new Vector2(8, 3) * 32);
+            waypoints.Enqueue(new Vector2(9, 3) * 32);
+            waypoints.Enqueue(new Vector2(10, 3) * 32);
+            waypoints.Enqueue(new Vector2(11, 3) * 32);
+            waypoints.Enqueue(new Vector2(12, 3) * 32);
+            waypoints.Enqueue(new Vector2(12, 4) * 32);
+            waypoints.Enqueue(new Vector2(12, 5) * 32);
+            waypoints.Enqueue(new Vector2(12, 6) * 32);
+            waypoints.Enqueue(new Vector2(12, 7) * 32);
+            waypoints.Enqueue(new Vector2(12, 8) * 32);
+            waypoints.Enqueue(new Vector2(12, 9) * 32);
+            waypoints.Enqueue(new Vector2(12, 10) * 32);
+            waypoints.Enqueue(new Vector2(12, 11) * 32);
+            waypoints.Enqueue(new Vector2(12, 12) * 32);
+            waypoints.Enqueue(new Vector2(12, 13) * 32);
+            waypoints.Enqueue(new Vector2(12, 14) * 32);
+
+
+        }
+
+        public Queue<Vector2> Waypoints
+        {
+            get { return waypoints; }
+        }
+
+        public void AddTexture(Texture2D texture)
+        {
+            tileTextures.Add(texture);
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    int textureIndex = map[y, x];
+                    if (textureIndex == -1)
+                        continue;
+
+                    Texture2D texture = tileTextures[textureIndex];
+                    batch.Draw(texture, new Rectangle(
+                        x * 32, y * 32, 32, 32), Color.White);
+                }
+            }
+        }
+
+        public int GetIndex(int cellX, int cellY)
+        {
+            if (cellX < 0 || cellX > Width || cellY < 0 || cellY > Height) return 0;
+            return map[cellY, cellX];
+        }
+
+    }
+}
---
+++ TowerDefenseXNA/WindowsGame1/WindowsGame1/WindowsGame1/Player.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace WindowsGame1
+{
+    class Player
+    {
+        private int gold = 50;
+        private int lives = 30;
+        private List<Tower> towers = new List<Tower>();
+        private MouseState mouseState; // Mouse state for the current frame
+        private MouseState oldState; // Mouse state for the previous frame
+        private Level level;
+        private int cellX;
+        private int cellY;
+        private int tileX;
+        private int tileY;
+        private Texture2D towerTexture;
+        private Texture2D bulletTexture;
+
+        public Player(Level level, Texture2D towerTexture, Texture2D bulletTexture)
+        {
+            this.level = level;
+            this.towerTexture = towerTexture;
+            this.bulletTexture = bulletTexture;
+        }
+        public void Update(GameTime gameTime, List<Enemy> enemies)
+        {
+            mouseState = Mouse.GetState();
+
+            cellX = (int)(mouseState.X / 32); // Convert the position of the mouse
+            cellY = (int)(mouseState.Y / 32); // from array space to level space
+
+            tileX = cellX * 32; // Convert from array space to level space
+            tileY = cellY * 32; // Convert from array space to level space
+            if(mouseState.LeftButton == ButtonState.Released &&
+                oldState.LeftButton == ButtonState.Pressed)
+            {
+                if (IsCellClear())
+                {
+
+                    ArrowTower tower = new ArrowTower(towerTexture,
+                     bulletTexture, new Vector2(tileX, tileY));
+
+                    towers.Add(tower);
+
+                }
+            }
+            foreach (Tower tower in towers)
+            {
+                if (tower.Target == null)
+                {
+                    tower.GetClosestEnemy(enemies);
+                }
+
+                tower.Update(gameTime);
+            }
+
+            oldState = mouseState; // Set the oldState so it becomes the state of the previous frame.
+        }
+
+
+        public int Gold
+        {
+            get { return gold; }
+        }
+        public int Lives
+        {
+            get { return lives; }
+        }
+        public Player(Level level)
+        {
+            this.level = level;
+        }
+
+        private bool IsCellClear()
+        {
+            bool inBounds = cellX >= 0 && cellY >= 0 &&
+                cellX < level.Width && cellY < level.Height;
+            bool spaceClear = true;
+            foreach (Tower tower in towers) // Check that there is no tower here
+            {
+                spaceClear = (tower.Position != new Vector2(tileX, tileY));
+
+                if (!spaceClear)
+                    break;
+            }
+            bool onPath = (level.GetIndex(cellX, cellY) != 1);
+            return inBounds && spaceClear && onPath;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (Tower tower in towers)
+            {
+                tower.Draw(spriteBatch);
+            }
+        }
+    }
+}
