b5de3dde0acf3b6c22a5241f8eacf3abcada91c1
Georgi Kirilov <gkkirilov@gmail.com>
2015-02-14 12:47:32 +0200

    Towers

---
+++ TowerDefenseGame/TowerDefenseGame/Models/ArrowTower.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Forms;
+
+namespace TowerDefenseGame.Models.Towers
+{
+    class ArrowTower :Tower
+    {
+        
+        private const int TowerSpeed = 10;
+        private const int TowerRadius = 400;
+        private const string TowerEffect = "ArrowTower";
+
+        public ArrowTower(int x, int y) : base(x, y, 50, 50, ArrowTower.TowerSpeed, ArrowTower.TowerRadius,ArrowTower.TowerEffect, Brushes.LawnGreen)
+        {
+
+        }
+
+        
+    }
+}
---
+++ TowerDefenseGame/TowerDefenseGame/Models/Tower.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Documents;
+using System.Windows.Media;
+using TowerDefenseGame.Geometry;
+using TowerDefenseGame.Interfaces;
+
+
+namespace TowerDefenseGame.Models.Towers
+{
+    abstract class Tower : GameObject
+    {
+        private int towerSpeed;
+        private int towerRadius;
+        private string towerEffect;
+
+        protected Tower(int x, int y, int width, int height, int TowerSpeed,int TowerRadius,string TowerEffect, Brush fillBrush)
+            : base(x, y, width, height, fillBrush)
+        {
+            this.towerSpeed = TowerSpeed;
+            this.towerRadius = TowerRadius;
+            this.towerEffect = TowerEffect;
+        }
+
+        public int TowerSpeed
+        {
+            get
+            {
+                return this.towerSpeed;
+            }
+            set
+            {
+                this.towerSpeed = value;
+            }
+        }
+
+        public int TowerRadius
+        {
+            get
+            {
+                return this.towerRadius;
+            }
+            set
+            {
+                this.towerRadius = value;
+            }
+        }
+
+        public string TowerEffect
+        {
+            get
+            {
+                return this.towerEffect;
+            }
+            set
+            {
+                this.towerEffect = value;
+            }
+        }
+
+
+    }
+}
