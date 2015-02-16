f132a7243fa75caac7abbf1769dcca53b39eddb4
mihayloff <mihayloff14@gmail.com>
2015-02-16 02:18:56 +0200

    implemented tower rotation towards its target.

--- TowerDefenseGame/TowerDefenseGame/Geometry/Point.cs
+++ TowerDefenseGame/TowerDefenseGame/Geometry/Point.cs
@@ -42,6 +42,64 @@
 
         public static void HandleMovement(Point objectPosition, Point targetPosition, int speed)
         {
+            //Point temp = new Point(objectPosition.X, objectPosition.Y + 5);
+            //
+            //double sideA = Math.Sqrt(Math.Pow(temp.X - targetPosition.X, 2) + Math.Pow(temp.Y - targetPosition.Y, 2));
+            //double sideB = Math.Sqrt(Math.Pow(objectPosition.X - targetPosition.X, 2) + Math.Pow(objectPosition.Y - targetPosition.Y, 2));
+            //double sideC = Math.Sqrt(Math.Pow(temp.X - objectPosition.X, 2) + Math.Pow(temp.Y - objectPosition.Y, 2));
+            //// cos A = ( b^2 + c^2 - a^2 ) / ( 2 bc )
+            //double angle = Math.Acos((Math.Pow(sideB, 2) + Math.Pow(sideC, 2) - Math.Pow(sideA, 2)) / (2 * sideB * sideC)) * 180 / Math.PI;
+            //
+            //if (angle == 90) 
+            //{
+            //    if (objectPosition.X > targetPosition.X)
+            //    {
+            //        objectPosition.X -= speed;
+            //    }
+            //    else
+            //    {
+            //        objectPosition.X += speed;
+            //    }
+            //}
+            //else if (angle == 180) 
+            //{
+            //    if (objectPosition.Y > targetPosition.Y)
+            //    {
+            //        objectPosition.Y -= speed;
+            //    }
+            //    else
+            //    {
+            //        objectPosition.Y += speed;
+            //    }
+            //}
+            //else if (angle < 90) 
+            //{
+            //    if (objectPosition.X > targetPosition.X)
+            //    {
+            //        objectPosition.X -= speed;
+            //        objectPosition.Y += (double)angle * 90 / 10000 * speed;
+            //    }
+            //    else
+            //    {
+            //        objectPosition.X += speed;
+            //        objectPosition.Y += (double)angle * 90 / 10000 * speed;
+            //    }
+            //}
+            //else
+            //{
+            //    if (objectPosition.Y > targetPosition.Y)
+            //    {
+            //        objectPosition.Y -= speed;
+            //        objectPosition.X += (double)(angle - 90) * 90 / 10000 * speed;
+            //    }
+            //    else
+            //    {
+            //        objectPosition.Y += speed;
+            //        objectPosition.X += (double)(angle - 90) * 90 / 10000 * speed;
+            //    }
+            //}
+            //
+            //return;
             if (targetPosition.X > objectPosition.X && targetPosition.Y == objectPosition.Y)
             {
                 objectPosition.X += speed; // Right
--- TowerDefenseGame/TowerDefenseGame/Models/Towers/Tower.cs
+++ TowerDefenseGame/TowerDefenseGame/Models/Towers/Tower.cs
@@ -3,7 +3,9 @@
     using System;
     using System.Linq;
     using System.Windows.Media;
+    using System.Windows.Media.Imaging;
     using TowerDefenseGame.Controllers;
+    using TowerDefenseGame.Core;
     using TowerDefenseGame.Models.Enemies;
     using TowerDefenseGame.Models.Projectiles;
 
@@ -13,11 +15,17 @@
         private int towerRange;
         private int frameCount = 0;
         private Enemy target;
+        private double playerAngle = 0;
+        private double lastAngle;
+        private double rotationBlendFactor = 0.05;
 
         // private string TowerEffect; - We shall implement this when we make the Effects class
         protected Tower(double x, double y, int width, int height, int towerSpeed, int towerRange, Brush fillBrush)
             : base(x, y, width, height, fillBrush)
         {
+            // For debugging reasons - mihayloff
+            // new ImageBrush(new BitmapImage(
+            // new Uri(@"D:\Programming\Repositories\SoftAvengers Game\Images\heart.png", UriKind.Relative)))
             this.TowerSpeed = towerSpeed;
             this.TowerRange = towerRange;
         }
@@ -84,6 +92,10 @@
             {
                 this.frameCount++;
             }
+
+            this.CalculateRotationAngle();
+            RotateTransform rotateTransform = new RotateTransform(90.0 - (this.playerAngle * 180 / Math.PI), Constants.FieldSegmentSize / 2, Constants.FieldSegmentSize / 2);
+            this.Model.RenderTransform = rotateTransform;
         }
 
         private void GetTarget()
@@ -98,6 +110,32 @@
                 .First();
 
             this.Target = targetSelected;
+        }
+
+        private void CalculateRotationAngle()
+        {
+            if (this.Target == null)
+            {
+                return;
+            }
+            double deltaX = (this.Coordinates.X + Constants.FieldSegmentSize / 2) -
+                (this.Target.Coordinates.X + Constants.FieldSegmentSize / 2);
+
+            double deltaY = (this.Coordinates.Y + Constants.FieldSegmentSize / 2) -
+                (this.Target.Coordinates.Y + Constants.FieldSegmentSize / 2);
+
+            double angle = Math.Atan2(deltaX, deltaY);
+
+            if (this.lastAngle < -2.0 && angle > 2.0)
+            {
+                this.playerAngle += Math.PI * 2.0;
+            }
+            else if (this.lastAngle > 2.0 && angle < -2.0)
+            {
+                this.playerAngle -= Math.PI * 2.0;
+            }
+            this.lastAngle = angle;
+            this.playerAngle = angle * this.rotationBlendFactor + this.playerAngle * (1 - this.rotationBlendFactor);
         }
     }
 }
