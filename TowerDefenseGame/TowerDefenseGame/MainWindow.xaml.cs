4edda370658000a2ba9ef510793610c33ca96052
Veselin Savov <veselin.savov1@gmail.com>
2015-02-24 16:31:50 +0200

    Added image bar, on tower select show information about tower

--- TowerDefenseGame/TowerDefenseGame/MainWindow.xaml.cs
+++ TowerDefenseGame/TowerDefenseGame/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace TowerDefenseGame
+﻿using System.Windows.Forms;
+
+namespace TowerDefenseGame
 {
     using System.Media;
     using System.Linq;
@@ -26,8 +28,8 @@
             AnimationController.ConfigureRenderer(this.MainCanvas);
             this.engine = new Engine();
             GameFieldController.SetGameFieldEvents(this);
-            SoundPlayer snd = new SoundPlayer(@"..\..\Resources\music.wav");
-            snd.PlayLooping();
+            //SoundPlayer snd = new SoundPlayer(@"..\..\Resources\music.wav");
+            //snd.PlayLooping();
         }
 
         public void GameFieldMouseLeftButtonDown(object sender, MouseEventArgs e)
@@ -57,7 +59,7 @@
                     }
                 }
             }
-            
+
 
             for (int col = 0; col < Constants.FieldCols; col++)
             {
@@ -83,9 +85,9 @@
 
         void TowerMouseButtonDown(object sender, MouseButtonEventArgs e)
         {
-            ((Rectangle) sender).Stroke = Brushes.Red;
-            ((Rectangle) sender).StrokeThickness = 2;
-            
+            ((Rectangle)sender).Stroke = Brushes.Red;
+            ((Rectangle)sender).StrokeThickness = 2;
+
             ((Rectangle)sender).RadiusX = 15;
             ((Rectangle)sender).RadiusY = 15;
 
@@ -106,7 +108,6 @@
                     this.towerImageFrame.RadiusY = 5;
                     this.towerImageFrame.RadiusX = 5;
                     this.towerImageFrame.StrokeThickness = 5;
-                    //PlayerInterfaceController.TowerInfo = TowerController.Towers[index];
 
                     return;
                 }
@@ -168,7 +169,7 @@
             RemoveButton.RadiusX = 3;
             RemoveButton.RadiusY = 3;
             PlayerInterfaceController.DestroySelectedTower();
-            
+
         }
 
         private void RemoveClicekdOnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
